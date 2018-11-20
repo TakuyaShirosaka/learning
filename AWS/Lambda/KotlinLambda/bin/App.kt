f05fa6bb6b7339ac4ad53be30322f6616fdb95cb
Takuya Shirosaka <pppg1023023@gmail.com>
2018-11-20 22:54:11 +0900

    KotlinでLambda

---
+++ AWS/Lambda/KotlinLambda/bin/App.kt
@@ -0,0 +1,15 @@
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.LambdaLogger
+
+/**
+ * Created by sakemotoshinya on 16/11/25.
+ */
+public class App {
+	public fun handler(count: Int, context: Context): String {
+		val lambdaLogger = context.getLogger()
+		lambdaLogger.log("Kotlinモジュールですよ！！！\n")
+		lambdaLogger.log("count = " + count +"\n")
+		lambdaLogger.log("インプットの3倍の値が出てきますよ！、テストパラメタは全て第一引数に入ってくるようです。\n")
+		return "${count * 3}"
+	}
+}
